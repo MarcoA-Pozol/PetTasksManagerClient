e0498591962589630295f999f13814ce95c8dcca
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-05-02 10:46:37 -0600

    Basic first design and style for Task cards

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,24 +1,23 @@
+import '../static/styles/taskCard.css';
+
 type TaskCardProps = {
     title: string;
     description: string;
     status: 'pending' | 'in-progress' | 'done';
-  };
-  
-const statusColors = {
-    pending: 'rgb(120, 80, 20)',
-    'in-progress': 'rgb(90, 20, 150)',
-    done: 'rgb(40, 140, 20)',
 };
   
 export default function TaskCard({ title, description, status }: TaskCardProps) {
 return (
-    <div>
-        <h3>{title}</h3>
-        <p >{description}</p>
-        <span style={{color:statusColors[status], fontWeight:"bold"}}>
-            {status.replace('-', ' ')}
-        </span>
-    </div>
-);
+        <div className={`task-card ${status}`}>
+            <h3 className="task-title">{title}</h3>
+            <p className="task-description">{description}</p>
+            <span className="task-status">{status.replace("-", " ")}</span>
+
+            <div className="task-actions">
+                <button className="start-btn">Begin Task</button>
+                <button className="complete-btn">Mark as completed</button>
+            </div>
+        </div>
+    );
 }
   
