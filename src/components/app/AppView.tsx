ff873fa3fd79f3cfac7d841ea253927e4e13ace2
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-05-03 06:53:58 -0600

    Redirect to authentication view if user is not authenticated.

--- src/components/app/AppView.tsx
+++ src/components/app/AppView.tsx
@@ -1,20 +1,38 @@
-import TaskCard from "./TaskCard";
 import { useState } from "react";
+import { useNavigate, Navigate } from "react-router-dom";
+import TaskCard from "./TaskCard";
 
 const AppView = () => {
     // Dark | Light theme
     const [theme, setTheme] = useState("light");
+    // Current auth user
+    const currentAuthUser = localStorage.getItem('currentAuthUser') || 'anonym';
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem('currentAuthUser');
+        navigate('/auth');
+    };
 
     const toggleTheme = () => {
         setTheme(theme === "light" ? "dark" : "light");
     };
 
+    // Redirect if the user is not authenticated
+
+ 
+
+    if (currentAuthUser === 'anonym') {
+        return <Navigate to='/auth'/>;
+    }
+ 
+
+
     return (
         <>
             <body className={`app-container ${theme}`}>
-                <button onClick={toggleTheme} className="theme-toggle-btn">
-                    Theme
-                </button>
+                <button onClick={toggleTheme} className="theme-toggle-btn">Theme</button>
+                <button onClick={handleLogout} style={{backgroundColor:'red', borderRadius:'5px', color:'white'}}>Logout</button>
                 
                 <TaskCard title="Remove Tailwind" description="Tailwind is not neccesary, it adds complexity." status="done" themeMode={theme}/>
                 <TaskCard title="Start login UI" description="Design login formulary style" status="in-progress" themeMode={theme}/>
