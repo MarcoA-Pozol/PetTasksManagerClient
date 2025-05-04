15913a1dc9daca923e9d115f71e66552d3b55fc0
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-05-03 23:08:56 -0600

    Navigate to AppView page after login was intended but not working yet.

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../../styles/auth/authForm.css';
 
 type Props = {
@@ -7,11 +8,25 @@ type Props = {
 
 const LoginForm: React.FC<Props> = ({children}:Props) => {
 
+    const handleFormSubmision = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget);
+        const username = formData.get("username") as string;
+        const password = formData.get("password") as string;
+
+        localStorage.setItem('currentAuthUser', username);
+
+        // Redirect to AppView
+        const navigate = useNavigate();
+        navigate('/');
+    };
+
     return(
         <>
-            <form className="auth-form">
+            <form className="auth-form" onSubmit={handleFormSubmision}>
                 <h2 className="auth-title">Welcome Back!</h2>
-                <input type="email" placeholder="Email" required className="auth-input" />
+                <input type="text" placeholder="Username" required className="auth-input" />
                 <input type="password" placeholder="Password" required className="auth-input" />
                 <button type="submit" className="auth-button">Login</button>
                 {children}
