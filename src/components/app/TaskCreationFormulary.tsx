a80c0c4edcc15e36158ac6d2bca3a07044f0d441
CsarNarciso <cesarpazol1029@gmail.com>
2025-05-16 21:15:55 -0600

    Removed description, deadline and repeat fields and translations for TaskCreationFormulary

--- src/components/app/TaskCreationFormulary.tsx
+++ src/components/app/TaskCreationFormulary.tsx
@@ -18,9 +18,6 @@ const TaskCreationFormulary = ({onData, userId}: TaskCreationFormularyProps) => 
     
     const { t } = useTranslation();
     const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-    const [deadline, setDeadline] = useState("");
-    const [repeat, setRepeat] = useState("");
 
     //Create new task on form submision
     const handleFormSubmision = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -49,9 +46,6 @@ const TaskCreationFormulary = ({onData, userId}: TaskCreationFormularyProps) => 
             <form className={`create-task-form`} onSubmit={handleFormSubmision}>
                 <h2>{t("Create a Task")}</h2>
                 <input type="text" placeholder={t("Title")} value={title} onChange={(e) => setTitle(e.target.value)}></input>
-                <input type="text" placeholder={t("Description")} value={description} onChange={(e) => setDescription(e.target.value)}></input>
-                <input type="text" placeholder={t("Deadline")}  value={deadline} onChange={(e) => setDeadline(e.target.value)}></input>
-                <input type="text" placeholder={t("Repeat")}  value={repeat} onChange={(e) => setRepeat(e.target.value)}></input>
                 <button type="submit">{t("Create")}</button>
             </form>
         </>
