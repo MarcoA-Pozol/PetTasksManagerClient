0fbb324069f970aab3d16c6693a23d1e9cf147bf
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-05-20 11:17:11 -0600

    Style for separators in tasks container and display of content structure

--- src/components/app/TasksContainer.tsx
+++ src/components/app/TasksContainer.tsx
@@ -4,39 +4,44 @@ import { TasksContainerProps } from "../../schemas/Task";
 const TasksContainer = ({theme, completedTasksList, uncompletedTasksList, removeTaskFromListOnCompleted, removeTaskFromListOnDeletion, diminishUncompletedTasksCount, diminishCompletedTasksCount, increaseCompletedTasksCount}:TasksContainerProps) => {
     return (
         <div className="middle-content">
-            <span>-- To do --</span>
-            {uncompletedTasksList.length === 0 ? (
-                <p>Free of tasks!</p> //If no uncompleted tasks...
-            ) : (
-                uncompletedTasksList.map((task) => (
-                    <TaskCard
-                        taskId={task._id}
-                        title={task.name}
-                        status="to-do"
-                        themeMode={theme}
-                        onComplete={() => {removeTaskFromListOnCompleted(task); diminishUncompletedTasksCount(); increaseCompletedTasksCount();}}
-                        onDelete={() => {removeTaskFromListOnDeletion(task._id, "to-do"); diminishUncompletedTasksCount();}}
-                        key={task._id} //element key for easier rendering (reacts knows what element remove, re-rendered).
-                    />
-                ))
-            )}   
+            <div className="uncompleted-tasks-container">
+                <span id="separator-to-do">-- To do --</span>
+                {uncompletedTasksList.length === 0 ? (
+                    <p style={{textAlign:"center", marginTop:"30px"}}>Free of tasks!</p> //If no uncompleted tasks...
+                ) : (
+                    uncompletedTasksList.map((task) => (
+                        <TaskCard
+                            taskId={task._id}
+                            title={task.name}
+                            status="to-do"
+                            themeMode={theme}
+                            onComplete={() => {removeTaskFromListOnCompleted(task); diminishUncompletedTasksCount(); increaseCompletedTasksCount();}}
+                            onDelete={() => {removeTaskFromListOnDeletion(task._id, "to-do"); diminishUncompletedTasksCount();}}
+                            key={task._id} //element key for easier rendering (reacts knows what element remove, re-rendered).
+                        />
+                    ))
+                )}   
+            </div>
 
-            <span>-- Completed --</span>
-            {completedTasksList.length === 0 ? (
-                <p>Nothing here</p> //If no completed tasks...
-            ) : (
-                completedTasksList.map((task) => (
-                    <TaskCard
-                        taskId={task._id}
-                        title={task.name}
-                        status="done"
-                        themeMode={theme}
-                        onComplete={() => {}} //Completed tasks can't be 'completed'!
-                        onDelete={() => {removeTaskFromListOnDeletion(task._id, "done"); diminishCompletedTasksCount();}}
-                        key={task._id}
-                    />
-                ))
-            )}     
+            <div className="completed-tasks-container">
+                <span id="separator-done">-- Done --</span>
+                {completedTasksList.length === 0 ? (
+                    <p style={{textAlign:"center", marginTop:"30px"}}>Nothing here</p> //If no completed tasks...
+                ) : (
+                    completedTasksList.map((task) => (
+                        <TaskCard
+                            taskId={task._id}
+                            title={task.name}
+                            status="done"
+                            themeMode={theme}
+                            onComplete={() => {}} //Completed tasks can't be 'completed'!
+                            onDelete={() => {removeTaskFromListOnDeletion(task._id, "done"); diminishCompletedTasksCount();}}
+                            key={task._id}
+                        />
+                    ))
+                )}     
+            </div>
+
         </div>
     );
 }
