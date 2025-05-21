35f83a550a3101b83ef9f73787f0cff06caa47e9
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-05-20 20:51:56 -0600

    Task separators extend or compress tasks containers depending their tasks state

--- src/components/app/TasksContainer.tsx
+++ src/components/app/TasksContainer.tsx
@@ -1,11 +1,32 @@
 import TaskCard from "./TaskCard";
 import { TasksContainerProps } from "../../schemas/Task";
+import { useState } from "react";
 
 const TasksContainer = ({theme, completedTasksList, uncompletedTasksList, removeTaskFromListOnCompleted, removeTaskFromListOnDeletion, diminishUncompletedTasksCount, diminishCompletedTasksCount, increaseCompletedTasksCount}:TasksContainerProps) => {
+    const [extendedContainer, setExtendedContainer] = useState("to-do");
+    const [uncompletedTskCtnStyle, setUncompletedTskCtnStyle] = useState("etd-tsk-ctn");
+    const [completedTskCtnStyle, setCompletedTskCtnStyle] = useState("cps-tsk-ctn");
+
+
+    // Compress and Extend tasks containers (to-do, done)
+    const toggleTaskContainers = () => {
+        setExtendedContainer(extendedContainer === "to-do" ? "done" : "to-do");
+
+        if (extendedContainer ===  "to-do") {
+            setUncompletedTskCtnStyle("etd-tsk-ctn");
+            setCompletedTskCtnStyle("cps-tsk-ctn");
+        } 
+
+        if (extendedContainer === "done") {
+            setUncompletedTskCtnStyle("cps-tsk-ctn");
+            setCompletedTskCtnStyle("etd-tsk-ctn");
+        }
+    };
+
     return (
         <div className="middle-content">
-            <div className="uncompleted-tasks-container">
-                <span id="separator-to-do">-- To do --</span>
+            <div className={`uncompleted-tasks-container ${uncompletedTskCtnStyle}`}>
+                <span id="separator-to-do" onClick={toggleTaskContainers}>To do</span>
                 {uncompletedTasksList.length === 0 ? (
                     <p style={{textAlign:"center", marginTop:"30px"}}>Free of tasks!</p> //If no uncompleted tasks...
                 ) : (
@@ -23,8 +44,8 @@ const TasksContainer = ({theme, completedTasksList, uncompletedTasksList, remove
                 )}   
             </div>
 
-            <div className="completed-tasks-container">
-                <span id="separator-done">-- Done --</span>
+            <div className={`completed-tasks-container ${completedTskCtnStyle}`}>
+                <span id="separator-done" onClick={toggleTaskContainers}>Done</span>
                 {completedTasksList.length === 0 ? (
                     <p style={{textAlign:"center", marginTop:"30px"}}>Nothing here</p> //If no completed tasks...
                 ) : (
