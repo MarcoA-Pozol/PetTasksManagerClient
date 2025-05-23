4cd0d2d3282de68f3c9abde02769f1c82798a2e4
CsarNarciso <cesarpazol1029@gmail.com>
2025-05-22 19:48:55 -0600

    Fix done tasks container not reacting after first click on separator element

--- src/components/app/TasksContainer.tsx
+++ src/components/app/TasksContainer.tsx
@@ -3,30 +3,29 @@ import { TasksContainerProps } from "../../schemas/Task";
 import { useState } from "react";
 
 const TasksContainer = ({theme, completedTasksList, uncompletedTasksList, removeTaskFromListOnCompleted, removeTaskFromListOnDeletion, diminishUncompletedTasksCount, diminishCompletedTasksCount, increaseCompletedTasksCount}:TasksContainerProps) => {
-    const [extendedContainer, setExtendedContainer] = useState("to-do");
+    const [currentExtendedContainer, setCurrentExtendedContainer] = useState("to-do");
     const [uncompletedTskCtnStyle, setUncompletedTskCtnStyle] = useState("etd-tsk-ctn");
     const [completedTskCtnStyle, setCompletedTskCtnStyle] = useState("cps-tsk-ctn");
 
 
     // Compress and Extend tasks containers (to-do, done)
-    const toggleTaskContainers = () => {
-        setExtendedContainer(extendedContainer === "to-do" ? "done" : "to-do");
+    const toggleTaskContainers = (target: 'to-do' | 'done') => {
+        
+        //Prevent clicking the same container
+        if(currentExtendedContainer === target) return;
+        
+        //Hide no selected container
+        setUncompletedTskCtnStyle(target === "to-do" ? "etd-tsk-ctn" : "cps-tsk-ctn");
+        setCompletedTskCtnStyle(target === "done" ? "etd-tsk-ctn" : "cps-tsk-ctn");
 
-        if (extendedContainer ===  "to-do") {
-            setUncompletedTskCtnStyle("etd-tsk-ctn");
-            setCompletedTskCtnStyle("cps-tsk-ctn");
-        } 
-
-        if (extendedContainer === "done") {
-            setUncompletedTskCtnStyle("cps-tsk-ctn");
-            setCompletedTskCtnStyle("etd-tsk-ctn");
-        }
+        //Updates in React occurs after function has been executed!
+        setCurrentExtendedContainer(target);
     };
 
     return (
         <div className="middle-content">
             <div className={`uncompleted-tasks-container ${uncompletedTskCtnStyle}`}>
-                <span className="tasks-separator" onClick={toggleTaskContainers} style={{backgroundColor:"rgb(104, 37, 180)"}}>To do</span>
+                <span className="tasks-separator" onClick={() => toggleTaskContainers("to-do")} style={{backgroundColor:"rgb(104, 37, 180)"}}>To do</span>
                 {uncompletedTasksList.length === 0 ? (
                     <p style={{textAlign:"center", marginTop:"30px"}}>Free of tasks!</p> //If no uncompleted tasks...
                 ) : (
@@ -45,7 +44,7 @@ const TasksContainer = ({theme, completedTasksList, uncompletedTasksList, remove
             </div>
 
             <div className={`completed-tasks-container ${completedTskCtnStyle}`}>
-                <span className="tasks-separator" onClick={toggleTaskContainers}  style={{backgroundColor:"rgb(11, 151, 11)"}}>Done</span>
+                <span className="tasks-separator" onClick={() => toggleTaskContainers("done")}  style={{backgroundColor:"rgb(11, 151, 11)"}}>Done</span>
                 {completedTasksList.length === 0 ? (
                     <p style={{textAlign:"center", marginTop:"30px"}}>Nothing here</p> //If no completed tasks...
                 ) : (
