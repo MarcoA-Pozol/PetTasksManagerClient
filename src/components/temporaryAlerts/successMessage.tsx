0358193efdb43e88dab320ba1147042f79907519
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-06-19 08:37:54 -0600

    Set successMessage component

--- src/components/temporaryAlerts/successMessage.tsx
+++ src/components/temporaryAlerts/successMessage.tsx
@@ -1,8 +1,11 @@
-const successMessage = () => {
+import { useTranslation } from "react-i18next";
+
+export const successMessage = (message:string) => {
+    const { t } = useTranslation();
 
     return (
-        <>
-        
-        </>
+        <div style={{backgroundColor:"#4caf50"}}>
+            {t(`${message}`)}
+        </div>
     );
 }
